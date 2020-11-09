84190362a8d3cd4a5a303fded3edf6dc542cbb39
AnshuVij <vijanshu27@gmail.com>
2020-11-09 16:57:59 +0530

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Mobile Apps Team on 11/9/20.
+//  Copyright © 2020 anshu vij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
